0724495fcec842d0d1887a7e34fb9271e2f06edc
Michael Huso <husomichael@gmail.com>
2021-11-06 14:08:27 -0500

    handleEmployeeTable working but appending wrong

--- client.js
+++ client.js
@@ -21,14 +21,31 @@ function handleSubmitButton(){
     employee.title = $('#title-input').val();
     employee.annualSalary = Number($('#annual-salary-input').val());
     console.log('in handleSubmitButton');
-    console.log(employee);
+    // console.log(employee);
     employees.push(employee);
-
     $('#first-name-input').val('');
     $('#last-name-input').val('');
     $('#id-input').val('');
     $('#title-input').val('');
     $('#annual-salary-input').val('');
+
+    handleEmployeeTable(employees);
 }
 
-$('#whatever')
+function handleEmployeeTable(employeesToRender){
+    $('#employee-table-body').empty();
+    // console.log('in handleEmployeeTable');
+    for(employee of employeesToRender){
+        let newTableRow = `
+            <tr>
+            <td>${employee.firstName}</td>
+            <td>${employee.lastName}</td>
+            <td>${employee.id}</td>
+            <td>${employee.title}</td>
+            <td>${employee.annualSalary}</td>
+            </tr>
+        `;
+        console.log(newTableRow);
+        $('#employee-table-body').append(newTableRow);
+    }
+}
