4e5dcfd877beb82c5a36688aff5fbff24055bdc4
Michael Huso <husomichael@gmail.com>
2021-11-06 12:37:59 -0500

    handleSubmitButton pushing employee to employees array working

--- client.js
+++ client.js
@@ -9,6 +9,15 @@ function readyNow(){
 }
 
 function handleSubmitButton(){
-    console.log('in handleSubmitButton');
+    let employee = {};
+    employee.firstName = $('#first-name-input').val();
+    employee.lastName = $('#last-name-input').val();
+    employee.id = $('#id-input').val();
+    employee.title = $('#title-input').val();
+    employee.annualSalary = Number($('#annual-salary-input').val());
+    // console.log('in handleSubmitButton');
+    // console.log(employee);
+    employees.push(employee);
 }
 
+$('#whatever')
