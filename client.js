7d8321bb32d8952decf7e09839d0b94e39244530
Michael Huso <husomichael@gmail.com>
2021-11-06 12:05:06 -0500

    handleSubmitButton linked up

--- client.js
+++ client.js
@@ -1,1 +1,14 @@
 console.log('js');
+
+$(document).ready(readyNow);
+
+let employees = [];
+
+function readyNow(){
+    $('#submit-button').on('click', handleSubmitButton);
+}
+
+function handleSubmitButton(){
+    console.log('in handleSubmitButton');
+}
+
